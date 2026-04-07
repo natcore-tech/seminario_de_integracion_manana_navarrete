31669fc49d3a27b5800a446537d0c1b753ae1e31
natcore-tech <elihu.navarrete@ute.edu.ec>
2026-04-07 11:00:00 -0500

    clase-07-04-2026

---
+++ modulo1/python_basics/01_hello_world.py
@@ -0,0 +1,12 @@
+print("Hello world")
+
+edad=20
+nombre='Natanael'
+print(edad)
+print(nombre)
+
+print(nombre, edad, end=' | ')
+print(nombre, edad, sep=' | ')
+
+print(f"{3.14159:.2f}")
+print(f"{3.14159:,}")
---
+++ modulo1/python_basics/04_operadores.py
@@ -0,0 +1,35 @@
+
+a=30
+b=5
+print ("Suma")
+print (a,"+",b,"=",a+b)
+
+print ("Resta")
+print (a,"-",b,"=",a-b)
+
+print ("Multiplicacion")
+print (a,"*",b,"=",a*b)
+
+
+
+print ("Operadores de Asignacion")
+print ("========================")
+n=10
+
+n+=5
+print("n+5", n)
+
+n-=3
+print("n-3", n)
+
+n*=3
+print("n*3", n)
+
+n//=3
+print("n//3", n)
+
+n%=3
+print("n%3", n)
+
+n**=3
+print("n**3", n)
